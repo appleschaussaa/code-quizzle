dd5f14fee06e7cb28c5f9d04ec6f2b8ebf0c6c50
Robert <robertschauss91@gmail.com>
2022-12-01 00:20:01 -0800

    creted index.html, style.css and script.js files. also listed out steps to complete.

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// create button to start quiz
+// create a click function for each of the 4 provided answers
+// view highscores should lead to highscore page
+// create button function for Submit, go back and highscore
+// have correct or wrong show up depending on answer
+
