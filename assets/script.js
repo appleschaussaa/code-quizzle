556806d6ac3698c51dcce1269300a75093aff701
Robert <robertschauss91@gmail.com>
2022-12-05 22:28:11 -0800

    salvaged what I could with the Javascript, Finished my README.md and small touches to everything else

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,59 @@
-// create button to start quiz
-// create a click function for each of the 4 provided answers
-// view highscores should lead to highscore page
-// create button function for Submit, go back and highscore
-// have correct or wrong show up depending on answer
-
 //button variables
-var answersA = document.querySelector(".buttonA");
-var answersB = document.querySelector(".buttonB");
-var answersC = document.querySelector(".buttonC");
-var answersD = document.querySelector(".buttonD");
-var startScreen = document.querySelector(".start-button");
-var highscoreInitials = document.querySelector(".initials");
+var answersA = document.getElementsByClassName("buttonA");
+var answersB = document.getElementsByClassName("buttonB");
+var answersC = document.getElementsByClassName("buttonC");
+var answersD = document.getElementsByClassName("buttonD");
+var mainStart = document.getElementsByClassName("start-button");
+var highscoreInitials = document.getElementsByClassName("initials");
 
 //other variables
-var currentQuestion = document.querySelector(".questions")
 var quizOver;
 var countdownTimer;
+var currentQuestion = document.querySelector(".questions");
+var mainScreen = document.getElementsByClassName("start-screen");
 
-startScreen.addEventListener("click", startQuiz);
+//adding click event
 answersA.addEventListener("click");
 answersB.addEventListener("click");
 answersC.addEventListener("click");
 answersD.addEventListener("click");
 highscoreInitials.addEventListener("click");
+mainStart.addEventListener("click", startQuiz);
 
-document.querySelector(".start-button").addEventListener("click", disappear);
+// begin by trying to give a click event to the start button
+document.querySelector(".start-button").addEventListener("click", changePage);
 
-function disappear() {
+// when the start button is pressed the start screen was supposed to be hidden
+function changePage() {
     document.querySelector(".start-screen").style.display = "none";
-}
+};
 
+function startQuiz() {
+    mainScreen.classList.add('hide');
+
+};
+
+// create function to display new question and answers on each press until it cycles through or time runs out
+// .sort(() => Math.random() is used somewhere in here from previous examples
+
+// create function that randomly chooses questions and the answers that go with them
+
+//create function that answers with correct or incorrect, if incorrect time will be subtracted
+
+// create function that keeps track of corrct and incorrect and adds it to the highscore-input and makes the display for that visible
+
+//create a function to clear all scores and a back button that goes back to the start screen, making it visible again while hiding the highscore screen
+
+// will use to change question within class="questions"
 function currentQuestion() {
-    "Commonly used data types DO NOT include: " , "The condition in an if/else statement is enclosed within ___. ", "Arrays in JavaScript can be used to store ___. ", "String values must be enclosed within ___ when being assigned to variables. ", "A very useful tool used during development and debugging for printing content to the debugger is: "
+    var questionInsert = ("Commonly used data types DO NOT include: " , "The condition in an if/else statement is enclosed within ___. ", "Arrays in JavaScript can be used to store ___. ", "String values must be enclosed within ___ when being assigned to variables. ", "A very useful tool used during development and debugging for printing content to the debugger is: ");
 }
+// will use to insert into each of the answer buttons
+function allAnswers () {
+    var answerOne = ("strings", "quotes", "numbers and strings", "commas", "JavaScript");
+    var answerTwo = ("booleans", "curly brackets", "other arrays", "curly brackets", "terminal/bash");
+    var answerThree = ("alerts", "parentheses", "booleans", "quotes", "for loops");
+    var answerFour = ("numbers", "square brackets", "all of the above", "parentheses", "consol.log");
+};
 
-function answerOne () {
-    "strings", "quotes", "numbers and strings", "commas", "JavaScript"
-}
-function answerTwo () {
-    "booleans", "curly brackets", "other arrays", "curly brackets", "terminal/bash"
-}
-function answersThree () {
-    "alerts", "parentheses", "booleans", "quotes", "for loops"
-}
-function answersFour () {
-    "numbers", "square brackets", "all of the above", "parentheses", "consol.log"
-}
+//
