96896243aa2223de9ca712764fdafe6882fa3213
Robert <robertschauss91@gmail.com>
2023-04-20 23:50:00 -0700

    quiz can run through all the way but still cannot get it to display a score and initials. Trying different ways to do it.

--- assets/script.js
+++ assets/script.js
@@ -3,29 +3,30 @@ var answersA = document.getElementById("buttonA");
 var answersB = document.getElementById("buttonB");
 var answersC = document.getElementById("buttonC");
 var answersD = document.getElementById("buttonD");
+var finalScore = document.getElementById("displayScore");
 var mainStart = document.querySelector(".start-button");
 var buttonAnswers = document.querySelectorAll(".buttonAnswers");
-// var highscoreInitials = document.getElementsByClassName("initials");
 var submitInitials = document.querySelector(".button-initials");
 var quizRestart = document.querySelector(".restart-quiz");
 var clearHighscores = document.querySelector(".clear-highscores");
+var addInitials = document.querySelector(".highscore-list");
 
-
-//other variables
 var quizOver;
 var timer;
 var timeLeft;
 var startQuestion = 0;
-// var currentQuestion = document.querySelector(".questionArea");
+var score = 0;
+var initials = "";
+var highscores = [];
 var countdownInsert = document.querySelector(".countdown-timer");
 var currentQuestion = document.getElementById("currentQuestions");
+var highscoreList = document.getElementById("highscore-list");
 var mainScreen = document.querySelector(".homepageQuiz");
 var quizSection = document.querySelector(".quiz-section");
 var highscoreInitials = document.querySelector(".highscore-input");
 var highscoreScreen = document.querySelector(".highscore-screen");
 var header = document.querySelector(".header");
 
-// function currentQuestion() {
 var questions = [
     { 
         question: "Commonly used data types DO NOT include: ",
@@ -33,7 +34,7 @@ var questions = [
         answerTwo: "booleans",
         answerThree: "alerts",
         answerFour: "numbers",
-        // correct: questions.answerThree
+        correct: "alerts"
     },
     { 
         question: "The condition in an if/else statement is enclosed within ___. ",
@@ -41,7 +42,7 @@ var questions = [
         answerTwo: "curly brackets",
         answerThree: "parenthesis",
         answerFour: "square brackets",
-        // correct: answerTwo
+        correct: "curly brackets"
     },
     { 
         question: "Arrays in JavaScript can be used to store ___. ",
@@ -49,7 +50,7 @@ var questions = [
         answerTwo: "other arrays",
         answerThree: "booleans",
         answerFour: "all of the above",
-        // correct: answerFour
+        correct: "all of the above"
     },
     { 
         question: "String values must be enclosed within ___ when being assigned to variables. ",
@@ -57,7 +58,7 @@ var questions = [
         answerTwo: "curly brackets",
         answerThree: "quotes",
         answerFour: "parenthesis",
-        // correct: answerThree
+        correct: "quotes"
     },
     { 
         question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
@@ -65,46 +66,11 @@ var questions = [
         answerTwo: "terminal/bash",
         answerThree: "for loops",
         answerFour: "console.log",
-        // correct: answerFour
+        correct: "console.log"
     }
 ];
 
 var finalQuestion = questions.length -1;
-
-//     var correctAnswer1 = answerOne[3];
-//     var correctAnswer2 = answerTwo[1];
-//     var correctAnswer3 = answerThree[3];
-//     var correctAnswer4 = answerFour[2];
-//     var correctAnswer5 = answerFive[3]
-// // };
-
-// function correctAnswer () {
-//     if (questionInsert[0].clickevent = correctAnswer1) {
-//         console.log("correct")
-//     } else {
-//         console.log("incorrect")
-//     };
-//     if (questionInsert[1].clickevent = correctAnswer2) {
-//         console.log("correct")
-//     } else {
-//         console.log("incorrect")
-//     };
-//     if (questionInsert[2].clickevent = correctAnswer3) {
-//         console.log("correct")
-//     } else {
-//         console.log("incorrect")
-//     };
-//     if (questionInsert[3].clickevent = correctAnswer4) {
-//         console.log("correct")
-//     } else {
-//         console.log("incorrect")
-//     };
-//     if (questionInsert[4].clickevent = correctAnswer5) {
-//         console.log("correct")
-//     } else {
-//         console.log("incorrect")
-//     };
-// };
 
 mainStart.addEventListener("click", startQuiz);
 
@@ -129,57 +95,120 @@ function countdownTimer() {
 
 function displayQuiz() {
     var questionDisplay = questions[startQuestion];
-    currentQuestion.innerHTML = "<h1>" + questionDisplay.question + "<h1>";
+    currentQuestion.innerHTML = "<h1>" + questionDisplay.question + "</h1>";
     answersA.innerHTML = "1. " + questionDisplay.answerOne;
     answersB.innerHTML = "2. " +questionDisplay.answerTwo;
     answersC.innerHTML = "3. " +questionDisplay.answerThree;
     answersD.innerHTML = "4. " +questionDisplay.answerFour;
-    buttonAnswers.forEach((btn) => {
-        btn.addEventListener("click", nextQuestion);
-    });
+    // buttonAnswers.forEach((btn) => {
+    //     btn.addEventListener("click", nextQuestion);
+    // });
 };
 
-// buttonAnswers.forEach((btn) => {
-//     btn.addEventListener("click", nextQuestion);
-// });
+buttonAnswers.forEach((btn) => {
+    btn.addEventListener("click", nextQuestion);
+});
 
-function nextQuestion() {
+var selectedAnswer = buttonAnswers.textContent;
+
+function nextQuestion(event) {
     if (startQuestion < finalQuestion) {
         startQuestion++;
-        return displayQuiz(startQuestion);
+        answerChoice(event);
     } else {
         quizSection.classList.add("hide");
-        // highscoreInitials.classList.remove("hide");
-        return highScoreInput()
+        highScoreInput();
     }
-    // return displayQuiz(startQuestion);
-}
-
-function highScoreInput() {
-    timer = clearInterval(timeLeft = 0)
-    header.classList.add("hide");
-    highscoreInitials.classList.remove("hide");
-    submitInitials.addEventListener("click", displayHighscore);
 };
 
-// submitInitials.addEventListener("click", displayHighscore);
+function answerChoice(event) {
+    var selectedAnswer = event.target.textContent;
+    var correctAnswer = questions[startQuestion].correct;
+    if (selectedAnswer === correctAnswer) {
+        score++;
+        console.log("Answer was correct");
+        // alert("Correct!");
+    } else {
+        timeLeft -= 5;
+        // alert("Incorrect!");
+        console.log("Answer was incorrect");
+    }
+    displayQuiz();
+};
+
+function highScoreInput() {
+    highscoreInitials.classList.remove("hide");
+    var inputElement = document.getElementById("highScoreInput");
+    if (inputElement && inputElement.value) {
+        var initials = inputElement.value;
+        clearInterval(timeLeft = 0)
+        header.classList.add("hide");
+        finalScore.textContent = "Your final score is " +score;
+    // var initials = document.getElementById("initials").value;
+    // highscoreList.innerHTML += "<p> " + initials + " - " + score + " </p>";
+        highscores.push({ initials: initials, score: score });
+        highscores.sort(function(a, b) {
+            return b.score - a.score;
+        });
+        // submitInitials.addEventListener("click", function() {
+        //     displayHighscore();
+        // });
+    };
+};
+
+// function displayHighscore() {
+//     // var initials = document.getElementById("initials").value;
+//     // highscoreList.innerHTML += "<p> " + initials + " - " + score + " </p>";
+//     highscoreInitials.classList.add("hide");
+//     highscoreScreen.classList.remove("hide");
+//     highscoreList.innerHTML = "";
+//     for (var i = 0; i < highscores.length; i++) {
+//         var highscore = highscores[i];
+//         highscoreList.innerHTML += "<p>" + highscore.initials + " - " + highscore.score + "</p>";
+//     }
+//     // highscoreList.innerHTML += "<p> " + initials + " - " + score + " </p>";
+//     clearHighscores.addEventListener("click", eraseHighscore);
+//     quizRestart.addEventListener("click", restartQuiz);
+// };
+
+submitInitials.addEventListener("click", function() {
+    displayHighscore();
+});
 
 function displayHighscore() {
     highscoreInitials.classList.add("hide");
     highscoreScreen.classList.remove("hide");
+    highscoreList.innerHTML = "";
+    for (var i = 0; i < highscores.length; i++) {
+        var listItem = document.createElement("li");
+        listItem.textContent = highscores[i].initials + " - " + highscores[i].score;
+        highscoreList.appendChild(listItem);
+    }
     clearHighscores.addEventListener("click", eraseHighscore);
     quizRestart.addEventListener("click", restartQuiz);
 };
 
-function eraseHighscore() {
+// submitInitials.addEventListener("click", function() {
+//     displayHighscore();
+// });
 
+function eraseHighscore() {
+    highscores = [];
+    highscoreList.innerHTML = ""; 
 };
 
 function restartQuiz() {
-    // highscoreScreen.classList.add("hide");
-    // mainScreen.classList.remove("hide");
-    // header.classList.remove("hide");
+    location.reload()
     // startQuestion = 0;
-    // timer = 0;
-    location.reload();
-}
+    // score = 0;
+    // highscores = [];
+    // highscoreList.innerHTML = "";
+    // timeLeft = 75;
+    // mainScreen.classList.remove("hide");
+    // quizSection.classList.add("hide");
+    // highscoreInitials.classList.add("hide");
+    // highscoreScreen.classList.add("hide");
+    // header.classList.remove("hide");
+    // clearInterval(timer);
+    // countdownInsert.textContent = "";
+};
